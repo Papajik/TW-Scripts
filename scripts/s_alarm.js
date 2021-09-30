835e07e687f04e3d1f5b1538e1c13f9eb7e050c7
Michal Papaj <michal.papaj@nangu.tv>
2021-09-30 16:34:49 +0200

    Added more settings with saving feature

--- scripts/s_alarm.js
+++ scripts/s_alarm.js
@@ -9,6 +9,9 @@ const locales = {
         'current_max': 'Current Maximum:',
         'trigger_at': 'Trigger at:',
         'storage_cap': ' % of storage',
+        'iron': 'Check iron',
+        'wood': 'Check wood',
+        'stone': 'Check stone',
     },
     'cs_CZ': {
         'volume_set': 'Hlasitost:',
@@ -16,6 +19,9 @@ const locales = {
         'current_max': 'Současné maximum:',
         'trigger_at': 'Spustit při:',
         'storage_cap': ' % skladu',
+        'iron': 'Hlídat železo',
+        'wood': 'Hlídat dřevo',
+        'stone': 'Hlídat hlínu',
     }
 };
 
@@ -30,29 +36,57 @@ if (locales.hasOwnProperty(locale)) {
 /*
 Variables
  */
+const local_storage_key = "s_alarm";
 const audio = new Audio(
     'https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
 let storageCap = 0.5;
 let countdown = 10;
+let s_settings;
+
 
 /*
  Main Script
  */
 createScriptPanel();
-setVolume(50);
+loadSettings();
 startIntervalAlarm().then();
 
 /**
  * Functions
  */
 
+function loadSettings() {
+    s_settings = JSON.parse(localStorage.getItem(local_storage_key));
+    if (!s_settings) s_settings = {'vol': 50, 'iron': true, 'wood': true, 'stone': true, 'cap': 50};
+    console.log(s_settings);
+    setVolume(s_settings['vol']);
+    setStorageCap(s_settings['cap']);
+    document.getElementById('s_a_w').checked = s_settings['wood'];
+    document.getElementById('s_a_s').checked = s_settings['stone'];
+    document.getElementById('s_a_i').checked = s_settings['iron'];
+    document.getElementById('s_cap').value = s_settings['cap'];
+    document.getElementById('s_vol').value = s_settings['vol'];
+}
+
+function setSettings(r, b) {
+    s_settings[r] = b;
+    saveSettings();
+}
+
+function saveSettings() {
+    localStorage.setItem(local_storage_key, JSON.stringify(s_settings));
+}
+
 function createScriptPanel() {
     let container = document.getElementById('contentContainer');
     let panel = `
     <table style="height: 100px; border: 1px solid rgba(128, 64, 0, 0.7); background-color: #fff8e6;" >
         <tbody>
-            <tr><td style="padding: 6px">${strings.volume_set}</td><td><span id="span_volume">.. %</span></td><td style="padding: 6px"><input type="range" min="0" max="100" value="50" oninput="setVolume(this.value)"></td></tr>
-            <tr><td style="padding: 6px">${strings.trigger_at}</td><td><span id="span_storage">50</span>${strings.storage_cap}</td><td style="padding: 6px"><input type="range" min="0" max="100" value="50" oninput="setStorageCap(this.value)"></td></tr>
+            <tr><td style="padding: 6px">${strings.volume_set}</td><td><span id="span_volume">.. %</span></td><td style="padding: 6px"><input id="s_vol" type="range" min="0" max="100" value="50" onchange="setSettings('vol', this.value)" oninput="setVolume(this.value)"></td></tr>
+            <tr><td style="padding: 6px">${strings.trigger_at}</td><td><span id="span_storage">.. %</span>${strings.storage_cap}</td><td style="padding: 6px"><input id="s_cap" type="range" min="0" max="100" value="$settings['cap']" onchange="setSettings('cap', this.value)" oninput="setStorageCap(this.value)"></td></tr>
+            <tr><td style="padding: 6px">${strings.wood}</td><td><input type="checkbox" id="s_a_w" onchange="setSettings('wood', this.checked)"><span class="icon header wood"> </span></td></tr>
+            <tr><td style="padding: 6px">${strings.stone}</td><td><input type="checkbox" id="s_a_s" onchange="setSettings('stone', this.checked)"><span class="icon header stone"> </td></tr>
+            <tr><td style="padding: 6px">${strings.iron}</td><td><input type="checkbox" id="s_a_i" onchange="setSettings('iron', this.checked)"><span class="icon header iron"> </td></tr>
             <tr><td style="padding: 6px">${strings.next_check}</td><td><span id="check_timer">10</span></td></tr>
             <tr><td style="padding: 6px">${strings.current_max}</td><td><span id="s_max">NaN</span></td></tr>
         </tbody>      
@@ -93,7 +127,7 @@ async function startIntervalAlarm() {
 
 function parseData(village) {
     let maxStorage = village['storage_max'];
-    let maxResources = Math.max(village['stone'], village['iron'], village['wood']);
+    let maxResources = Math.max(s_settings['stone'] ? village['stone'] : 0, s_settings['iron'] ? village['iron'] : 0, s_settings['wood'] ? village['wood'] : 0);
     document.getElementById('s_max').innerText = String(maxResources);
     if (maxResources > maxStorage * storageCap) {
         audio.play().then();
